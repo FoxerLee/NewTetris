7015cace67935cc93376e9813b576453370171e5
FoxerLee <foxerlee1@gmail.com>
2021-02-13 13:02:53 -0500

    change score

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public List<Obj> objPrefabList;
+    public Transform spawnPoint;
+    public Button playButton;
+    public Text scoreLabel;
+    public AudioSource collideAudio;
+    public int score;
+    // public int cubeLeft;
+
+    private Obj obj;
+    private int objId;
+    private bool isGameOver;
+
+    private List<Obj> objs = new List<Obj>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        // obj = SpawnNextObj();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGameOver)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            var mousePos = Input.mousePosition;
+            var worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            
+            var objPos = new Vector3(worldPos.x, spawnPoint.position.y, -1);
+            obj.gameObject.transform.position = objPos;
+
+            obj.SetSimulated(true);
+
+            obj = SpawnNextObj();
+        }
+    }
+
+    private Obj SpawnNextObj()
+    {
+        var rand = Random.Range(0, objPrefabList.Count-1);
+        var prefab = objPrefabList[rand].gameObject;
+        var pos = spawnPoint.position;
+        ChangeScore();
+
+        return SpawnObj(prefab, pos);
+    }
+
+    private Obj SpawnObj(GameObject prefab, Vector3 pos)
+    {
+
+        var obj = Instantiate(prefab, pos, Quaternion.identity);
+        var f = obj.GetComponent<Obj>();
+        f.SetSimulated(false);
+        f.id = objId++;
+        f.OnLevelUp = (a, b) =>
+        {
+            if (isObjExist(a) && isObjExist(b))
+            {
+                var pos1 = a.gameObject.transform.position;
+                var pos2 = b.gameObject.transform.position;
+                var pos_new = (pos1 + pos2) * 0.5f;
+                RemoveObj(a);
+                RemoveObj(b);
+                // AddScore(a.score);
+                // Instantiate(a.nextLevelPrefab, pos_new, Quaternion.identity);
+                collideAudio.Play();
+                var fr = SpawnObj(a.nextLevelPrefab, pos_new);
+                fr.SetSimulated(true);
+            }
+        };
+
+        f.OnGameOver = () =>
+        {
+            if (isGameOver == true)
+            {
+                return;
+            }
+            OnGameOver();
+        };
+        objs.Add(f);
+
+
+
+        return f;
+    }
+
+    public void Restart()
+    {
+        playButton.gameObject.SetActive(false);
+        obj = SpawnNextObj();
+        Debug.Log("Restart");
+        score = 50;
+        scoreLabel.text = "50";
+
+        isGameOver = false;
+    }
+
+    private void OnGameOver()
+    {
+        isGameOver = true;
+        playButton.gameObject.SetActive(true);
+
+        for (int i = 0; i < objs.Count; i++)
+        {
+            objs[i].SetSimulated(false);
+            // AddScore(objs[i].score);
+            Destroy(objs[i].gameObject);
+        }
+
+        objs.Clear();
+    }
+
+    private void RemoveObj(Obj f)
+    {
+        for (int i = 0; i < objs.Count; i++)
+        {
+            if (objs[i].id == f.id)
+            {
+                objs.Remove(f);
+                Destroy(f.gameObject);
+                return;
+            }
+        }
+    }
+
+    private bool isObjExist(Obj f)
+    {
+        for (int i = 0; i < objs.Count; i++)
+        {
+            if (objs[i].id == f.id)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void AddScore(int score)
+    {
+        this.score += score;
+        scoreLabel.text = $"{this.score}";
+    }
+
+    private void ChangeScore()
+    {
+        this.score -= 1;
+        scoreLabel.text = $"{this.score}";
+    }
+
+}
---
+++ Assets/Scripts/Obj.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+public class Obj : MonoBehaviour
+{
+    public int id;
+    public int score;
+    public GameObject nextLevelPrefab;
+    public Action<Obj, Obj> OnLevelUp;
+    public Action OnGameOver;
+
+    private Rigidbody2D rigid;
+    private bool isTouchRedline;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouchRedline == false)
+        {
+            return;
+        }
+        timer += Time.deltaTime;
+        if (timer > 5)
+        {
+            Debug.Log("Game Over!");
+            OnGameOver?.Invoke();
+        }
+    }
+
+    public void SetSimulated(bool b)
+    {
+        if (rigid == null)
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+        rigid.simulated = b;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var obj = collision.gameObject;
+        var obj_old = obj.GetComponent<Obj>();
+        if (obj.CompareTag("Obj"))
+        {   
+            if (obj.name == gameObject.name)
+            {
+                if (nextLevelPrefab != null)
+                {
+                    OnLevelUp?.Invoke(this, obj_old);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var obj = collision.gameObject;
+        if (obj.CompareTag("Redline"))
+        {
+            // Debug.Log("OnTriggerEnter2D Redline");
+            isTouchRedline = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var obj = collision.gameObject;
+        if (obj.CompareTag("Redline"))
+        {
+            // Debug.Log("OnTriggerExit2D Redline");
+            isTouchRedline = false;
+            timer = 0;
+        }
+    }
+
+}
